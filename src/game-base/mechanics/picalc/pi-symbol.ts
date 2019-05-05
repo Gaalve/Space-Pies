c278b38c9dbbe4606e0ce33f6b8a6ad6a90b891d
algorithmv3 <adrian.schwaiger@campus.tu-berlin.de>
2019-05-05 03:59:35 +0200

    added documentation to pi-symbol

--- src/game-base/mechanics/picalc/pi-symbol.ts
+++ src/game-base/mechanics/picalc/pi-symbol.ts
@@ -8,23 +8,54 @@ export abstract class PiSymbol {
         this.name = name;
     }
 
+    /**
+     * Return a custom representation of this symbol that should make its use clear!
+     * E.g.: x(a).getName() => x, but x(a).getFullName() => x(a)
+     */
     public abstract getFullName(): string;
 
+
+    /**
+     * Returns the name of this symbol.
+     * E.g.: x(a) => x
+     */
     public getName(): string{
         return this.name;
     }
 
+
+    /**
+     * Returns the complete sequence of symbols that may follow after this symbol.
+     * E.g.: x(a).z(b).P
+     */
     public getSymbolSequence(): string{
         return this.name;
     }
 
+    /**
+     * Should only be called by Pi-System!
+     *
+     * Gets called from PiSystem.phaseTriggerSymbols
+     * and triggers the action of the symbol (e.g. a PiProcess will call the specified callback)
+     */
     public trigger(): void{
-        // Should only be used by pi-process
+        // Default: Do nothing
     }
 
+    /**
+     * Should only be called by Pi-Channel-In!
+     *
+     * Binds the transferred name (argValue) to the free name (argName).
+     * E.g. x<a> | x(q) => argName:= q, argValue:= a
+     * @param argName
+     * @param argValue
+     */
     public rename(argName: string, argValue: string){
-        // should only be used by pi-action
+        // Default: Do nothing! TODO: should probably be overwritten by all symbols
     }
 
+    /**
+     * Creates a deep copy of this symbol (except for Pi-Term).
+     */
     public abstract copy(): PiSymbol;
 }
--- src/game-base/mechanics/picalc/pi-term.ts
+++ src/game-base/mechanics/picalc/pi-term.ts
@@ -1,10 +1,29 @@
 import {PiSymbol} from "./pi-symbol";
+import {PiSystem} from "./pi-system";
 
-export class PiTerm {
+export class PiTerm extends PiSymbol{
 
-    private sequentialSymbols: PiSymbol[];
-    private concurrentTerms: PiTerm[];
+    public symbol: PiSymbol;
 
+    public constructor(system: PiSystem, name: string, symbol: PiSymbol){
+        super(system, name.toUpperCase());
+        this.symbol = symbol;
+    }
 
+    copy(): PiTerm { // no deep copy in case of recursions!
+        return this;
+    }
+
+    getSymbolSequence(): string {
+        return this.getName() + ":= " + this.symbol.getSymbolSequence();
+    }
+
+    getFullName(): string {
+        return this.getName();
+    }
+
+    trigger(): void {
+        this.system.addSymbol(this.symbol);
+    }
 
 }
