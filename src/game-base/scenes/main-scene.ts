e59975b297683670ad1ae9f8d0fa66e36235d16a
Patrick <patrick_russ@posteo.de>
2019-05-15 20:58:08 +0200

    pi calculus terms included, ready to be tested with other classes

--- src/game-base/scenes/main-scene.ts
+++ src/game-base/scenes/main-scene.ts
@@ -92,12 +92,12 @@ export class MainScene extends Phaser.Scene {
         );
         this.buttonOption.setPosition(1750, 100);
         this.system.start();
-        //this.system.pushSymbol(this.system.add.channelOut("wext10l", "*").nullProcess());
-        //this.system.pushSymbol(this.system.add.channelOut("wmod1", "*").nullProcess());
-        //this.system.pushSymbol(this.system.add.channelOut("wmod1", "*").nullProcess());
-        //this.system.pushSymbol(this.system.add.channelOut("wext12l", "*").nullProcess());
-        //this.system.pushSymbol(this.system.add.channelOut("wext11p", "*").nullProcess());
-
+        /*this.system.pushSymbol(this.system.add.channelOut("wext10l", "*").nullProcess());
+        this.system.pushSymbol(this.system.add.channelOut("wmod1", "*").nullProcess());
+        this.system.pushSymbol(this.system.add.channelOut("wmod1", "*").nullProcess());
+        this.system.pushSymbol(this.system.add.channelOut("wext11l", "*").nullProcess());
+        this.system.pushSymbol(this.system.add.channelOut("wext11p", "*").nullProcess());
+        */
     }
 
 
