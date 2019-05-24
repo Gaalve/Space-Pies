1f7e9b614cc85a509ae438f7075588ae5c01369b
algorithmv3 <adrian.schwaiger@campus.tu-berlin.de>
2019-05-25 00:32:23 +0200

    adjusted explosions

--- src/game-base/mechanics/ship.ts
+++ src/game-base/mechanics/ship.ts
@@ -93,6 +93,28 @@ export class Ship{
         this.scene.time.delayedCall(14500, ()=>{this.modularShip.toDestroyedHull()}, [], this);
     }
 
+    private exploedP2(): void{
+        //TODO
+        this.scene.time.delayedCall(2000, this.explosion2At, [40, -130, 0.85, 2.4], this);
+        this.scene.time.delayedCall(2500, ()=>{this.modularShip.toDestroyedWingUp()}, [], this);
+
+        this.scene.time.delayedCall(5000, this.explosion2At, [40, 110, 0.85, 2.4], this);
+        this.scene.time.delayedCall(5050, this.explosion2At, [40, 0, 0.85, 1], this);
+        this.scene.time.delayedCall(5500, ()=>{this.modularShip.toDestroyedWingDown()}, [], this);
+
+        this.scene.time.delayedCall(8000, this.explosion2At, [-60, -100, 0.85, 2.4], this);
+        this.scene.time.delayedCall(8050, this.explosion2At, [-60, 100, 0.85, 2.4], this);
+        this.scene.time.delayedCall(8500, ()=>{this.modularShip.toDestroyedBack()}, [], this);
+
+
+        this.scene.time.delayedCall(11000, this.explosion2At, [165, 0, 0.85, 2.4], this);
+        this.scene.time.delayedCall(11500, ()=>{this.modularShip.toDestroyedPilot()}, [], this);
+
+        this.scene.time.delayedCall(14000, this.explosion2At, [20, 0, 0.85, 2.4], this);
+        this.scene.time.delayedCall(14080, this.explosion2At, [150, 0, 0.85, 1.2], this);
+        this.scene.time.delayedCall(14500, ()=>{this.modularShip.toDestroyedHull()}, [], this);
+    }
+
     private explosionAt(offX: number, offY: number, scale: number = 1): void{
         this.explosionSmoke.fromJSON({
             x: this.posX, y: this.posY,  tint: 0xff1a1a1a, speed: {min: 5, max: 120},radial: true,
@@ -111,10 +133,10 @@ export class Ship{
             scale: (particle, key, t) => t > 0.8 ? 1-(t - 0.8)*5 : 1,
             lifespan: {min: 1200*scale, max: 1400*scale}, on: false});
 
-        this.explosionSmoke.emitParticle(120 * scale, this.posX + offX, this.posY + offY);
-        this.explosionRed.emitParticle(120 * scale, this.posX + offX, this.posY + offY);
-        this.explosionOrange.emitParticle(120 * scale, this.posX + offX, this.posY + offY);
-        this.explosionYellow.emitParticle(120 * scale, this.posX + offX, this.posY + offY);
+        this.explosionSmoke.emitParticle(140 * scale, this.posX + offX, this.posY + offY);
+        this.explosionRed.emitParticle(100 * scale, this.posX + offX, this.posY + offY);
+        this.explosionOrange.emitParticle(80 * scale, this.posX + offX, this.posY + offY);
+        this.explosionYellow.emitParticle(60 * scale, this.posX + offX, this.posY + offY);
     }
 
     public explosion2At(offX: number, offY: number, lifeScale: number = 1, speedScale: number = 1): void{
@@ -135,10 +157,10 @@ export class Ship{
             scale: (particle, key, t) => t > 0.8 ? 1-(t - 0.8)*5 : 1,
             lifespan: {min: 1200*lifeScale, max: 1400*lifeScale}, on: false});
 
-        this.explosionSmoke.emitParticle(120 * lifeScale*speedScale, this.posX + offX, this.posY + offY);
-        this.explosionRed.emitParticle(120 * lifeScale*speedScale, this.posX + offX, this.posY + offY);
-        this.explosionOrange.emitParticle(120 * lifeScale*speedScale, this.posX + offX, this.posY + offY);
-        this.explosionYellow.emitParticle(120 * lifeScale*speedScale, this.posX + offX, this.posY + offY);
+        this.explosionSmoke.emitParticle(140 * lifeScale*speedScale, this.posX + offX, this.posY + offY);
+        this.explosionRed.emitParticle(100 * lifeScale*speedScale, this.posX + offX, this.posY + offY);
+        this.explosionOrange.emitParticle(80 * lifeScale*speedScale, this.posX + offX, this.posY + offY);
+        this.explosionYellow.emitParticle(60 * lifeScale*speedScale, this.posX + offX, this.posY + offY);
     }
 
 }
