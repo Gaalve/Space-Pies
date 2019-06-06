7ad46f8487d74e1364202d3a40eb871f3ab5823b
larsh <znt@gmx.de>
2019-06-06 08:00:58 +0200

    viel aufgeräumt und an die neue version angepasst

--- src/game-base/mechanics/turn.ts
+++ src/game-base/mechanics/turn.ts
@@ -1,8 +1,6 @@
 import {Player} from "./player";
-import {ShopSceneP1} from "../scenes/shop-sceneP1";
-import {PiCalcTests} from "../tests/pi-calc-tests";
 import {PiSystem} from "../mechanics/picalc/pi-system";
-import {chooseSceneP1} from "../scenes/choose-sceneP1";
+
 
 export class Turn {
     private refScene: Phaser.Scene;
@@ -28,7 +26,6 @@ export class Turn {
         this.awaitInput = false;
         this.currentRound = 0;
         this.refScene.data.set('currentPlayer', this.currentPlayer.getNameIdentifier());
-        //this.refScene.data.set('round', ""+(++this.currentRound));
         this.refScene.data.set('turnAction', 'Create');
         this.refScene.data.set('currentPlayer', this.currentPlayer.getNameIdentifier());
         this.refScene.data.set('round', ""+(this.currentRound));
@@ -49,59 +46,31 @@ export class Turn {
 
         }
 
-        let sys = this.currentPlayer.getPiSystem();
+      //  let sys = this.currentPlayer.getSystem();
+   /* this.system.pushSymbol(
+        this.system.add.replication(
+        this.system.add.channelIn('player2', '').channelOut('shopp2', '').
+        channelIn('shopp2end', '').channelOut('unlock2', '').
+        channelIn('attack2end', '').channelOut('player1', '').nullProcess()
+        )
+    ); */
 
-        sys.pushSymbol(
-            sys.add.replication(
-                sys.add.channelIn('player2', '').channelOut('shopp2', '').
-                channelIn('shopp2end', '').channelOut('unlock2', '').
-                channelIn('attack2end', '').channelOut('player1', '').nullProcess()
-            )
-        );
 
+    if(this.currentPlayer.getNameIdentifier() == "P1"){
+       this.system.pushSymbol(this.system.add.channelOut("shopp1", "*").nullProcess())
 
-        if(this.currentPlayer.getNameIdentifier() == "P1"){
-            //this.refScene.scene.run( 'ShopSceneP1');
-            // system.pushSymbol(startShop)
-          //  system.pushSymbol(system.add.channelOut('shopp1', '*').nullProcess())
-            //this.refScene.events.emit("newTurn")
-            //this.refScene.scene.get("MainScene").events.emit("newTurn");
-            this.system.pushSymbol(this.system.add.channelOut("shopp1", "*").nullProcess())
-
-            //  this.player.getSystem().pushSymbol(this.system().add.channelOut('shopp1', '*').nullProcess())
-            // sys.pushSymbol( sys.add.replication
-            // let player1turn = system.add.replication(system.add.channelIn('player1', '*').channelOut('shopp1', '').nullProcess());
-            // sys.pushSymbol( sys.add.replication( [ sys.add.chanIn('player1', '').chanOut('uin1', '')....]));
-        }
-        else {
-            this.system.pushSymbol(this.system.add.channelOut("shopp1", "*").nullProcess())
-        }
-        this.awaitInput = true; //nächster Spieler
-
-        this.refScene.data.set('turnAction', 'Shopping Phase');
+       }
+       else {
+          this.system.pushSymbol(this.system.add.channelOut("shopp1", "*").nullProcess())
+       }
+       this.awaitInput = true; //nächster Spieler
+       this.refScene.data.set('turnAction', 'Shopping Phase');
 
     }
 
     public Attackturn():void{
         if (!this.awaitInput) return;
         this.clickable = false;
-        /*this.refScene.scene.sleep('ShopSceneP1');
-        if(this.refScene.scene.get("chooseSceneP1").scene.isActive()){
-            this.refScene.scene.sleep('chooseSceneP1');
-        }
-        if(this.refScene.scene.get("chooseTypeSceneP1").scene.isActive()) {
-            this.refScene.scene.sleep('chooseTypeSceneP1');
-        }
-
-        if(this.currentRound != 1){
-            this.refScene.scene.sleep('ShopSceneP2');
-            if(this.refScene.scene.get("chooseSceneP2").scene.isActive()) {
-                this.refScene.scene.sleep('chooseSceneP2');
-            }
-            if(this.refScene.scene.get("chooseTypeSceneP2").scene.isActive()) {
-                this.refScene.scene.sleep('chooseTypeSceneP2');
-            }
-        }*/
         this.system.pushSymbol(this.system.add.channelOut("closeshop", "*").nullProcess());
 
         //Waffen schießen lassen:
