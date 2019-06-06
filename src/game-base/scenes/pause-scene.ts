e594d65b6ccd67da47511c8ed3430bee8c5f3596
m.schoppe <m.schoppe@campus.tu-berlin.de>
2019-06-07 01:05:37 +0200

    add Pause Hints

--- src/game-base/scenes/pause-scene.ts
+++ src/game-base/scenes/pause-scene.ts
@@ -40,7 +40,7 @@ export class PauseScene extends Phaser.Scene {
         this.P2 = this.scene.get('MainScene').data.get("P2");
 
         //this.add.image(1920/2, 1080/2, "background_space")
-        const titleText = this.add.text(1920/2-50, 200, 'Pause!', {
+        const titleText = this.add.text(1920/2-130, 200, 'Pause!', {
             fill: '#f0f1ff', fontFamily: '"Roboto"', fontSize: 60, fontStyle: 'bold', strokeThickness: 2
         });
 
@@ -51,9 +51,9 @@ export class PauseScene extends Phaser.Scene {
             this.scene.sleep();}
             //this.scene.setVisible(false,"PauseScene")}
         );
-        this.buttonResume.setPosition(1920/2, 1080/2-75);
+        this.buttonResume.setPosition(1920/2-130, 1080/2-75);
 
-        const resumeText = this.add.text(1920/2+80, 1080/2-100, "Resume", {
+        const resumeText = this.add.text(1920/2-60, 1080/2-100, "Resume", {
             fill: '#fff', fontFamily: '"Roboto"', fontSize: 42, strokeThickness: 2});
 
 
@@ -65,9 +65,9 @@ export class PauseScene extends Phaser.Scene {
             this.P1.resetEnergy();
             this.scene.sleep();});
 
-        this.buttonReset.setPosition(1920/2, 1080/2+75);
+        this.buttonReset.setPosition(1920/2-130, 1080/2+75);
 
-        const resetText = this.add.text(1920/2+80, 1080/2+50, "Reset", {
+        const resetText = this.add.text(1920/2-60, 1080/2+50, "Reset", {
             fill: '#fff', fontFamily: '"Roboto"', fontSize: 42, strokeThickness: 2});
 
         const debugText = this.add.text(1760, 1020, "Change Debug",{
@@ -77,6 +77,7 @@ export class PauseScene extends Phaser.Scene {
             fill: '#fff', fontFamily: '"Roboto"', fontSize: 20});
         debugState.setOrigin(0.5);
 
+
         this.buttonDebug = new Button(this, 100, 100, "button_shadow",
             "button_bg", "button_fg","button_options",
             ()=>{
@@ -85,6 +86,47 @@ export class PauseScene extends Phaser.Scene {
             })
 
         this.buttonDebug.setPosition(1880, 1040);
+
+
+
+        // Weapons Hints
+        this.add.text(120,150,'Waepon Hints:',{
+            fill: '#fff', fontFamily: '"Roboto"', fontSize: 28, strokeThickness: 2});
+
+
+        this.add.image(120,350,'ssr_weap_pro');
+        this.add.image(120,400,'ssb_weap_pro');
+        this.add.text(230,350+25,'Projectile Weapons to attack Armor-shields');
+
+        this.add.image(120,550,'ssr_weap_las');
+        this.add.image(120,600,'ssb_weap_las');
+        this.add.text(230,550+25,'Laser Weapons to attack energy-shields');
+
+        this.add.image(120,750,'ssr_weap_rock');
+        this.add.image(120,800,'ssb_weap_rock');
+        this.add.text(230,750+25,'Rocket launcher to destory all kinds of shields');
+
+
+        // Shield Hints
+
+        this.add.text(1920-400,150,'Shield Hints:',{
+            fill: '#fff', fontFamily: '"Roboto"', fontSize: 28, strokeThickness: 2});
+
+
+        this.add.image(1920-120,340,'button_shield');
+        this.add.text(1920-600,340,'Energy-shield protects from Laser Weapons,\nvisualis by blue bars\nPi-Kalk:');
+
+        this.add.image(1920-120,460,'button_armor');
+        this.add.text(1920-600,440,'Armor-shield protects from Projectile Weapons,\nvisualis by grey bars');
+
+        this.add.image(1920-120,580,'button_rocket');
+        this.add.text(1920-600,560,'Rocket-shield protects from Rocket launcher,\nvisualis by red bars');
+
+        this.add.image(1920-120,700,'button_nano');
+        this.add.text(1920-600,680,'Nano-shield cheap and weak shield,\nthat protects from all kinds of Weapons\nvisualis by braun bars');
+
+        this.add.image(1920-120,820,'button_adapt');
+        this.add.text(1920-600,800,'Super-shield expensive and strong shield,\nthat protects from all kinds of Weapons');
 
     }
 
