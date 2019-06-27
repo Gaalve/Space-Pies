8627267c83b21d62d49e792c1f8ceb520a9d1d4d
m.schoppe <m.schoppe@campus.tu-berlin.de>
2019-06-27 17:40:34 +0200

    change pause menu Pi-style button

--- src/game-base/mechanics/roundTimeBar.ts
+++ src/game-base/mechanics/roundTimeBar.ts
@@ -1,29 +1,42 @@
 export class roundTimeBar extends Phaser.GameObjects.Sprite{
 
-    private timer_length;
+
+    private timebarEvent;
+
+
 
     public constructor(scene : Phaser.Scene){
         super(scene, 0, 0, "battletime");
         this.x = 1920/2;
         this.y = 155;
-        this.setVisible(false)
+        this.setVisible(false);
         this.scene.add.existing(this);
-        this.timer_length=3000;
         this.active=false;
+
+
+
     }
 
-    public setTime() : void{
+    public setTimer(event) : void{
         this.active=true;
         this.visible=true;
         this.displayWidth = this.width;
+        this.timebarEvent=event;
+
+    }
+
+    public stopTimer():void{
+        this.timebarEvent.remove();
+        this.visible=false;
+        this.active=false;
+
     }
 
 
 
-    update(time, delta): void {
 
-        let duration;
-        //if(duration)
+    update(): void {
+        this.displayWidth=this.width*(1-this.timebarEvent.getProgress());
     }
 
 }
